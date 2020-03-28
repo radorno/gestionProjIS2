762bb17f1043a6ab948224698d36fcc4427face0
Ruth <rutieli25@gmail.com>
2020-03-28 18:43:44 -0400

    Add files via upload

---
+++ proyecto_Modificado/proyectoIs2/src/main/java/com/is2/web/app/controller/IndexControllers.java
@@ -0,0 +1,16 @@
+package com.is2.web.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app")
+public class IndexControllers {
+	
+	@GetMapping({"/index","/"})       
+	public String index(){
+		return "index";
+	}
+
+}
