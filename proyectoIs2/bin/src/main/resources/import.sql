0d57172704d6cec3e1644528e10fdede755b2222
Justo Almirón <jalmiron@consultoresinformaticos.com.py>
2020-08-04 16:16:56 -0400

    se agrego ajustes a login

---
+++ proyectoIs2/bin/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into roles values(1,'Administrador');
+insert into roles values(2,'Desarrollador');
+insert into roles values(3,'Admin. y Desarrollador');
--- proyectoIs2/src/main/resources/import.sql
+++ proyectoIs2/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 insert into roles values(1,'Administrador');
 insert into roles values(2,'Desarrollador');
 insert into roles values(3,'Admin. y Desarrollador');
+insert into usuarios(NOMBRE,APELLIDO,USER_CODE,PASSWORD,ID_ROL,FECHA_CREACION) values('admin','admin','admin','admin',1,'2020-01-11')
